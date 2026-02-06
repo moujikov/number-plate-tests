b53407d5aba7315d952ba941b3023c2210cc5bc2
Andrey Moujikov <a@moujikov.email>
2026-02-06 11:08:18 +0300

    Provide auth token error details

--- rest_server/common/auth.py
+++ rest_server/common/auth.py
@@ -12,4 +12,7 @@ if ACCESS_TOKEN:
 
 def check_authorized(access_token: str):
   if ACCESS_TOKEN and access_token != ACCESS_TOKEN:
-    raise HTTPException(status.HTTP_401_UNAUTHORIZED, headers={"WWW-Authenticate": "Bearer"})
+    raise HTTPException(
+      status.HTTP_401_UNAUTHORIZED,
+      detail="No valid access token provided",
+      headers={"WWW-Authenticate": "Bearer"})
