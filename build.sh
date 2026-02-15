984756118910702a97fd8f8723436b235e158c96
Andrey Moujikov <a@moujikov.email>
2026-02-15 21:18:29 +0300

    cleaning up

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-DOCKER_BUILDKIT=1 docker build -t moujikov/number-plates-all .
--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-docker run --rm -it \
-	-v .:/app \
-	-p 8000:8000 \
-	moujikov/number-plates-all
