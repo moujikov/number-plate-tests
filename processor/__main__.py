edea2aabe92335b9e8c9e3c738c9f5454b3070db
Andrey Moujikov <a@moujikov.email>
2026-03-10 19:25:23 +0300

    main loop refactored

--- processor/__main__.py
+++ processor/__main__.py
@@ -7,25 +7,24 @@ from .session import SchedulerSession
 from .processor import ImagesProcessor
 
 
-async def main_loop(interval, periodic_function):
-  await init_database()
-  while True:
-    # Repeat after the interval or when done, whatever comes LAST
-    await asyncio.gather(
-      asyncio.sleep(interval / 1000),
-      periodic_function())
-    
-async def cleanup():
-  await session.close()
-  await release_database()
 
+async def main():
+  try:
+    await init_database()
 
-session = SchedulerSession()
-processor = ImagesProcessor(session)
+    session = SchedulerSession()
+    processor = ImagesProcessor(session)
+
+    while True:   # Repeat after the interval or when done, whatever comes LAST
+      await asyncio.gather(
+        asyncio.sleep(CHECK_PERIOD / 1000),
+        processor.process_new_images())
+  finally:
+    if session: await session.close()
+    await release_database()
+
 
 try:
-  asyncio.run(main_loop(CHECK_PERIOD, processor.process_new_images))
+  asyncio.run(main())
 except KeyboardInterrupt:
   pass # Allows graceful exit
-finally:
-  asyncio.run(cleanup())
